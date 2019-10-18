4dad52ca64a90bfeda1913f453d8d9bf61a3863c
shashikant88093 <shashi94goswami@gmail.com>
2019-10-18 20:03:40 +0530

    heelo

---
+++ src/Containers/Blog/Blog.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+
+class Blog extends Component {
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+    }
+    render () {
+        return (
+            <div>
+                <section className="Posts">
+                    <Post />
+                    <Post />
+                    <Post />
+                </section>
+                <section>
+                    <FullPost />
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
